293b3230398cd9cda868348e2ad635cbbef5635c
acmirand <acmirand@uci.edu>
2017-03-01 00:23:57 -0800

    feat: Added Main Menu and Scene One
    -Main Menu: Start Button and Background
    -Scene One: When hit key "Esc" Pauses Game.

---
+++ Assets/_Scripts/Esc_Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esc_Pause : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject Main_Menu;
+
+	private bool screen_paused = false;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("escape") && screen_paused == false) {
+            Time.timeScale = 0;
+            Main_Menu.SetActive(true);
+            screen_paused = true;
+            
+        }
+        else if(Input.GetKeyDown("escape") && screen_paused == true) {
+            Time.timeScale = 1;
+            Main_Menu.SetActive(false);
+            screen_paused = false;
+        }
+	}
+}
---
+++ Assets/_Scripts/MenuBtnManagers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuBtnManagers : MonoBehaviour {
+
+    public void NewGameBtn(string NewGameLevel) {
+        SceneManager.LoadScene(NewGameLevel);
+    }
+}
