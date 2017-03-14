e762627aa73bdad5bd01cb4030d61d5577f3b5fc
Harry Wong <harry10286@gmail.com>
2017-03-13 20:43:34 -0700

    Feat: replaced players with cats Bug encountered: cats not picking up purple or green powerups

--- Assets/_Scripts/BulletController.cs
+++ Assets/_Scripts/BulletController.cs
@@ -44,8 +44,8 @@ public class BulletController : MonoBehaviour {
 
     private void Fire() {
         time = Time.time;
-        GameObject bullet_inst = Instantiate(shotPrefab, exit_Point.transform.position, Quaternion.identity);
-        bullet_inst.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.right * speed);
+		GameObject bullet_inst = Instantiate(shotPrefab, exit_Point.transform.position + new Vector3(1f,0.5f,-0f), Quaternion.identity);
+		bullet_inst.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * speed);
 
         --remainingBullets;
         updateBulletText();
